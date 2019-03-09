6d688b245db1cbd96e73a06501e2783ef47e4948
Josias Iquabius <iquabius@gmail.com>
2019-03-08 22:00:31 -0400

    Add console.log to snippet

--- somar-valores-do-extrado-no-guiabolso-web.js
+++ somar-valores-do-extrado-no-guiabolso-web.js
@@ -1,2 +1,2 @@
 // Soma valores do extrato no Guiabolso Web
-Array.prototype.slice.call(document.querySelectorAll('.sc-jtRfpW.hTaFtu .jss88 span')).map(sp => sp.innerText).map(val => val.replace(' R$ ', '').replace(',', '.')).map(v => Number.parseFloat(v)).reduce((acc, n) => (acc + n), 0);
+console.log(Array.prototype.slice.call(document.querySelectorAll('.sc-jtRfpW.hTaFtu .jss88 span')).map(sp => sp.innerText).map(val => val.replace(' R$ ', '').replace(',', '.')).map(v => Number.parseFloat(v)).reduce((acc, n) => (acc + n), 0));
